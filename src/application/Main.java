7ac6386ba0790ed65e2bba1e7cb7b6b0e148dd85
xznikos <nikolasmota2@gmail.com>
2026-01-30 20:16:17 -0300

    Department entity class

---
+++ src/application/Main.java
@@ -0,0 +1,10 @@
+package application;
+
+import model.entities.Department;
+
+public class Main {
+    static void main() {
+        Department obj = new Department(1, "books");
+        System.out.println(obj);
+    }
+}
