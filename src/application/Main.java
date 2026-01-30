29ea3ef7cab8b09a6426b52e57971cf5d843f77f
xznikos <nikolasmota2@gmail.com>
2026-01-30 20:36:40 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -9,6 +11,7 @@ public class Main {
     static void main() {
         Department obj = new Department(1, "RH");
         Seller fun1 = new Seller(1, "Nikolas", "nikolas@gmail", new Date(), 1200.0, obj);
+        SellerDao sellerDao = DaoFactory.createSellerDao();
         System.out.println(fun1);
     }
 }
