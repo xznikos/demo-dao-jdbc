77ead5fc8c70f146e698eab4e49cd2f32ebfe53d
xznikos <nikolasmota2@gmail.com>
2026-01-30 20:30:46 -0300

    Department and SellerDao interfaces

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,14 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Main {
     static void main() {
-        Department obj = new Department(1, "books");
-        System.out.println(obj);
+        Department obj = new Department(1, "RH");
+        Seller fun1 = new Seller(1, "Nikolas", "nikolas@gmail", new Date(), 1200.0, obj);
+        System.out.println(fun1);
     }
 }
