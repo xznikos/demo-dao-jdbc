d460b082766e576e102cbdc4e01f439e933f4071
xznikos <nikolasmota2@gmail.com>
2026-01-31 23:46:04 -0300

    findAll implementation

--- src/application/Main.java
+++ src/application/Main.java
@@ -22,5 +22,11 @@ public class Main {
         for (Seller obj : list){
             System.out.println(obj);
         }
+
+        System.out.println("\n==Find All: ");
+        list = sellerDao.findAll();
+        for (Seller obj : list){
+            System.out.println(obj);
+        }
     }
 }
